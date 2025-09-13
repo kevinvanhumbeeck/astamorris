ad13d4944779ac567d503ea12b8e31502c525cb8
Kevin Van Humbeeck <kevin.vanhumbeeck@mediahuis.be>
2025-09-13 11:37:53 +0000

    finished sell page

---
+++ mail.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Astamorris 2.0</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Kevin Van Humbeeck">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="container">
+        <img id="logo" src="img/logo.png" alt="Logo" width="850" height="383">
+        <hr />
+        <div class="desc">
+            Bedankt voor je bestelling! <br />
+            U mag <span class="highlight">&euro; </span> overschrijven op rekeningnummer <span class="highlight">BE12 3456 7890 1234</span> met vermelding <span class="highlight">Astamorris  </span>. <br />
+            U ontvangt een mail wanneer uw bestelling naar u werd verzonden of wanneer u deze kan ophalen.
+            <br><br>
+                    </div>
+    </body>
+</html>
